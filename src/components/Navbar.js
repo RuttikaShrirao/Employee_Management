401496aa8d97dfee9de28fadd85fb3e577f89692
RuttikaShrirao <ruttikashrirao@gmail.com>
2024-12-01 21:44:16 +0530

    navbar name uploaded

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => (
   <nav className="bg-blue-600 p-4 text-white">
-    <h1 className="text-xl font-bold">User Directory</h1>
+    <h1 className="text-xl font-bold">Employee Details</h1>
   </nav>
 );
 
